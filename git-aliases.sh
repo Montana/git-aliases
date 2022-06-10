35a9dca550366c80e418a6a09f8c9b50b8dec962
Montana Mendy <montana@linux.com>
2022-06-09 19:44:36 -0700

    remove null line

--- git-aliases.sh
+++ git-aliases.sh
@@ -22,4 +22,3 @@ git config --global alias.st 'status --short --branch'
 
 # git glog
 git config --global alias.glog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
- 
